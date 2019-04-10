5302951033080522b73024e0290b17afb83adcbb
F3FFO <federico.pieranton@gmail.com>
2019-04-10 22:16:21 +0200

    First Release

---
+++ app/src/main/java/com/f3ffo/hellobusbologna/BusBot.java
@@ -0,0 +1,8 @@
+package com.f3ffo.hellobusbologna;
+
+public class BusBot {
+    private String helloBusUrl = "https://hellobuswsweb.tper.it/web-services/hello-bus.asmx/QueryHellobus?fermata=%s&oraHHMM=%s&linea=%s";
+
+    public void searchBus(int busStation, int busLine) {
+    }
+}
