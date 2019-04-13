15643008d17a935a2b6c06e25c5d462b470e4432
Raphael <Raphael@192.168.178.47>
2019-04-13 18:21:15 +0200

    CreateCSVEstractor v.2

--- JavaApplication14/src/javaapplication14/JavaApplication13.java
+++ JavaApplication14/src/javaapplication14/JavaApplication13.java
@@ -1,17 +1,18 @@
 package javaapplication14;
 
-import java.awt.Desktop;
 import java.io.IOException;
-import java.net.URI;
-import java.net.URISyntaxException;
 
 public class JavaApplication13 {
 
-    public static void main(String[] args) throws IOException, URISyntaxException {
+    public static void main(String[] args) throws IOException {
         BusReader p = new BusReader();
+        UrlBusElaboration n = new UrlBusElaboration();
+
         p.fileToArrayList("C:/Users/Emanuele/Desktop/lineefermate_20190401 (2).csv");
         p.busCodeToPrint("");
         p.stopCodeToPrint("");
         p.stopNameToPrint("");
+
+        n.httpExtractOnlyStop("6035");
     }
 }
