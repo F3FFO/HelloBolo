92c864c205bfe45d6dc9050e4f42227d075db4e1
FEFFO <federico.pieranton@gmail.com>
2019-12-15 23:45:13 +0100

    Update preference

--- app/src/main/java/com/f3ffo/hellobusbologna/preference/PreferencesActivity.java
+++ app/src/main/java/com/f3ffo/hellobusbologna/preference/PreferencesActivity.java
@@ -1,15 +1,12 @@
 package com.f3ffo.hellobusbologna.preference;
 
-import android.content.SharedPreferences;
 import android.content.res.Configuration;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.app.AppCompatDelegate;
 import androidx.preference.PreferenceFragmentCompat;
-import androidx.preference.PreferenceManager;
 
 import com.f3ffo.hellobusbologna.R;
 
@@ -18,20 +15,8 @@ public class PreferencesActivity extends AppCompatActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        SharedPreferences mPrefs = PreferenceManager.getDefaultSharedPreferences(this);
-        if (mPrefs.getString(getString(R.string.preference_key_theme), "").equals("true")) {
-            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
-        } else if (mPrefs.getString(getString(R.string.preference_key_theme), "").equals("false")) {
-            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
-        } else {
-            if (Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.Q) {
-                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM);
-            } else {
-                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
-            }
-        }
-        int currentNightMode = getResources().getConfiguration().uiMode & Configuration.UI_MODE_NIGHT_MASK;
-        switch (currentNightMode) {
+        new Preference().getPreferenceTheme(PreferencesActivity.this);
+        switch (getResources().getConfiguration().uiMode & Configuration.UI_MODE_NIGHT_MASK) {
             case Configuration.UI_MODE_NIGHT_NO:
                 if (Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.M && Build.VERSION.SDK_INT < android.os.Build.VERSION_CODES.O) {
                     getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
