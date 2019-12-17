f6e15e00e0dbdbb1953b9e012e047dad70da3563
FEFFO <federico.pieranton@gmail.com>
2019-12-17 23:06:52 +0100

    Implement new class to obtain coordinates

---
+++ app/src/main/java/com/f3ffo/busbolo/position/Gps.java
@@ -0,0 +1,71 @@
+package com.f3ffo.busbolo.position;
+
+import android.content.Context;
+import android.location.Location;
+import android.location.LocationManager;
+import android.os.Looper;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+
+public class Gps {
+
+    private Context context;
+    private FusedLocationProviderClient mFusedLocationClient;
+    private double latitude, longitude;
+
+    public Gps(Context context) {
+        this.context = context;
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(context);
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    private boolean isLocationEnabled() {
+        LocationManager locationManager = (LocationManager) context.getSystemService(Context.LOCATION_SERVICE);
+        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) || locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+    }
+
+    private LocationCallback mLocationCallback = new LocationCallback() {
+        @Override
+        public void onLocationResult(LocationResult locationResult) {
+            Location mLastLocation = locationResult.getLastLocation();
+            latitude = mLastLocation.getLatitude();
+            longitude = mLastLocation.getLongitude();
+        }
+    };
+
+    private void requestNewLocationData() {
+        LocationRequest mLocationRequest = new LocationRequest();
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        mLocationRequest.setInterval(0);
+        mLocationRequest.setFastestInterval(0);
+        mLocationRequest.setNumUpdates(1);
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(context);
+        mFusedLocationClient.requestLocationUpdates(mLocationRequest, mLocationCallback, Looper.myLooper());
+    }
+
+    public void getLastLocation() {
+        if (isLocationEnabled()) {
+            mFusedLocationClient.getLastLocation().addOnCompleteListener(task -> {
+                Location location = task.getResult();
+                if (location == null) {
+                    requestNewLocationData();
+                } else {
+                    latitude = location.getLatitude();
+                    longitude = location.getLongitude();
+                }
+            });
+        }
+    }
+
+}
