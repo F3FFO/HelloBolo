5af14c8a80617a02fb4b768e34d99a28ccf12917
FEFFO <federico.pieranton@gmail.com>
2019-12-30 11:51:18 +0100

    Bug fix + add method theme

--- app/src/main/java/com/f3ffo/hellobolo/preference/PreferencesActivity.java
+++ app/src/main/java/com/f3ffo/hellobolo/preference/PreferencesActivity.java
@@ -6,18 +6,28 @@ import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.AppCompatImageButton;
+import androidx.preference.ListPreference;
 import androidx.preference.PreferenceFragmentCompat;
 
 import com.f3ffo.hellobolo.R;
+
+import org.apache.commons.io.FileUtils;
+
+import java.io.IOException;
 
 public class PreferencesActivity extends AppCompatActivity {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
-        new Preference().getPreferenceTheme(PreferencesActivity.this);
+        if (Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.Q) {
+            getString(R.string.preference_summary_theme_29, new Preference(PreferencesActivity.this).setPreferenceTheme());
+        } else {
+            getString(R.string.preference_summary_theme, new Preference(PreferencesActivity.this).setPreferenceTheme());
+        }
         super.onCreate(savedInstanceState);
         switch (getResources().getConfiguration().uiMode & Configuration.UI_MODE_NIGHT_MASK) {
             case Configuration.UI_MODE_NIGHT_NO:
@@ -48,11 +58,60 @@ public class PreferencesActivity extends AppCompatActivity {
         });
     }
 
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+    }
+
     public static class PreferencesFragment extends PreferenceFragmentCompat {
 
         @Override
         public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
-            setPreferencesFromResource(R.xml.preference, rootKey);
+            addPreferencesFromResource(R.xml.preference);
+            ListPreference listPreference = findPreference(getString(R.string.preference_key_theme));
+            listPreference.setOnPreferenceChangeListener((androidx.preference.Preference preference, Object newValue) -> {
+                //getActivity().recreate();
+                //getActivity().startActivity(new Intent(getContext(), PreferencesActivity.class).addFlags(Intent.FLAG_ACTIVITY_EXCLUDE_FROM_RECENTS).addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION));
+                Toast.makeText(getContext(), getString(R.string.toast_theme), Toast.LENGTH_LONG).show();
+                return true;
+            });
+            androidx.preference.Preference clearMemory = getPreferenceManager().findPreference(getString(R.string.preference_key_clear_memory));
+            if (clearMemory != null) {
+                clearMemory.setOnPreferenceClickListener((androidx.preference.Preference preference) -> {
+                    try {
+                        FileUtils.deleteDirectory(getContext().getFilesDir());
+                        Toast.makeText(getContext(), getString(R.string.toast_file_deleted), Toast.LENGTH_LONG).show();
+                        return true;
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                        Toast.makeText(getContext(), getString(R.string.toast_no_file_deleted), Toast.LENGTH_LONG).show();
+                        return false;
+                    }
+                });
+            }
+            /*androidx.preference.Preference button = getPreferenceManager().findPreference(getString(R.string.preference_key_gps));
+            if (button != null) {
+                button.setOnPreferenceClickListener((androidx.preference.Preference preference) -> {
+                    LinearLayoutCompat layout = new LinearLayoutCompat(getContext());
+                    layout.setOrientation(LinearLayoutCompat.VERTICAL);
+                    layout.setLayoutParams(new LinearLayoutCompat.LayoutParams(LinearLayoutCompat.LayoutParams.MATCH_PARENT, LinearLayoutCompat.LayoutParams.WRAP_CONTENT));
+                    Slider slider = new Slider(getContext());
+                    LinearLayoutCompat.LayoutParams lparams = new LinearLayoutCompat.LayoutParams(LinearLayoutCompat.LayoutParams.WRAP_CONTENT, LinearLayoutCompat.LayoutParams.WRAP_CONTENT);
+                    lparams.setMargins(128, 16, 128, 16);
+                    slider.setLayoutParams(lparams);
+                    slider.setValueFrom(0);
+                    slider.setValueTo(2);
+                    slider.setStepSize(1);
+                    layout.addView(slider);
+                    new MaterialAlertDialogBuilder(getContext(), R.style.AlertDialogTheme)
+                            .setTitle("Range")
+                            .setView(layout)
+                            .setNegativeButton(R.string.alertDialog_gps_no, (DialogInterface dialog, int which) -> dialog.dismiss())
+                            .setPositiveButton(R.string.alertDialog_gps_yes, (DialogInterface dialog, int which) -> new Preference(getContext()).setPreferenceGps(slider.getValue()))
+                            .show();
+                    return false;
+                });
+            }*/
         }
     }
 }
